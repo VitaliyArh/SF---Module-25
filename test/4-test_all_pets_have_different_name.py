d8bb01ae381b18f8dc83d844ee187ea89316a92c
VitaliyArh <vita173@yandex.ru>
2022-12-08 18:43:54 +0200

    Update 4-test_all_pets_have_different_name.py

--- test/4-test_all_pets_have_different_name.py
+++ test/4-test_all_pets_have_different_name.py
@@ -31,4 +31,4 @@ def test_all_pets_have_different_names(go_to_my_pets):
    print(r)
    print(pets_name)
 
-# python -m pytest -v --driver Chrome --driver-path c:/chromedriverchromedriver.exe tests/4-test_all_pets_have_different_name.py
+# python -m pytest -v --driver Chrome --driver-path C:/chromedriver/chromedriver.exe tests/4-test_all_pets_have_different_name.py
